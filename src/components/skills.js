ccf35df8c5da68d117724497cc7befacc520fe10
Anisha <anisha.0612@live.com>
2020-09-02 17:38:07 -0400

    edit about me

--- src/components/skills.js
+++ src/components/skills.js
@@ -7,7 +7,7 @@ const skills = [
   { id: 6, name: "SQL", value: 4 },
   { id: 7, name: "jQuery", value: 3.5 },
   { id: 8, name: "MongoDB", value: 3 },
-  { id: 9, name: "React.js", value: 2 },
+  { id: 9, name: "React.js", value: 3 },
   { id: 10, name: "Node.js", value: 2 },
 ];
 
--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -10,10 +10,10 @@ const AboutPage = () => {
       <img src={Profile} alt="profile" />
       <h1 className="about-header">HI THERE</h1>
       <h3 class="about-text">
-        I am an Aspiring <strong>Web Developer</strong> based in Wilmington,DE.
-        I am skillful at building Responsive Web applications. I am{" "}
+        I am a <strong>Web Developer</strong> based in Wilmington,DE. I am
+        skillful at building Responsive Web applications. I am{" "}
         <strong>passionate</strong> to learn and bring ideas to life . I believe
-        that one would <strong>pursue their passion</strong> and building Web
+        that one should <strong>pursue their passion</strong> and building Web
         applications is mine. I also believe that one should{" "}
         <strong>learn everyday</strong> to be better at what we do.
       </h3>
