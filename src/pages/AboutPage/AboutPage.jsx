c7f9013a96cb1cd2c03b9f19055add7587d12654
Anisha <anisha.0612@live.com>
2021-03-25 14:42:48 -0400

    edit aboutpage

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -5,82 +5,80 @@ import "./AboutPage.css";
 
 const About = () => {
   return (
-    <div className="AboutPage">
-      <img src={Profile} alt="profile" />
-      <h1 className="about-header">Hi There</h1>
-      <h3 className="about-text">
-        I am a <strong>Web Developer</strong> based in Delaware. I am skillful
-        at building Responsive Web applications. I am{" "}
-        <strong>passionate</strong> to learn and bring ideas to life . I believe
-        that one should <strong>pursue their passion</strong> and building Web
-        applications is mine. I also believe that one should{" "}
-        <strong>learn everyday</strong> to be better at what we do.
+    <div className='AboutPage'>
+      <img src={Profile} alt='profile' />
+      <h1 className='about-header'>Hi There</h1>
+      <h3 className='about-text'>
+        What makes me unique is I am self-driven. I have taught myself to build
+        and design websites. I am always learning, trying to build something
+        from scratch, researching the tools and technologies I use. I am
+        passionate about creating beautiful, performant applications with good
+        user experiences.
       </h3>
       <h2
-        className="center about-header"
-        style={{ paddingTop: "0.5em", fontSize: "2em" }}
-      >
+        className='center about-header'
+        style={{ paddingTop: "0.5em", fontSize: "2em" }}>
         Skills
       </h2>
-      <div className="row">
-        <div className="column">
+      <div className='row'>
+        <div className='column'>
           <img
-            src="https://img.icons8.com/color/144/000000/javascript.png"
-            alt="Javascript"
+            src='https://img.icons8.com/color/144/000000/javascript.png'
+            alt='Javascript'
           />
         </div>
-        <div className="column">
+        <div className='column'>
           <img
-            src="https://img.icons8.com/plasticine/200/000000/react.png"
-            alt="React.js"
+            src='https://img.icons8.com/plasticine/200/000000/react.png'
+            alt='React.js'
           />
         </div>
-        <div className="column">
+        <div className='column'>
           <img
-            src="https://img.icons8.com/color/144/000000/html-5.png"
-            alt="HTML5"
+            src='https://img.icons8.com/color/144/000000/html-5.png'
+            alt='HTML5'
           />
         </div>
-        <div className="column">
+        <div className='column'>
           <img
-            src="https://img.icons8.com/color/144/000000/css3.png"
-            alt="CSS3"
+            src='https://img.icons8.com/color/144/000000/css3.png'
+            alt='CSS3'
           />
         </div>
-        <div className="column">
+        <div className='column'>
           <img
-            src="https://img.icons8.com/color/144/000000/bootstrap.png"
-            alt="Bootstrap"
+            src='https://img.icons8.com/color/144/000000/bootstrap.png'
+            alt='Bootstrap'
           />
         </div>
-        <div className="column">
+        <div className='column'>
           <img
-            src="https://img.icons8.com/ultraviolet/120/000000/database-export.png"
-            alt="SQL"
+            src='https://img.icons8.com/ultraviolet/120/000000/database-export.png'
+            alt='SQL'
           />
         </div>
-        <div className="column">
+        <div className='column'>
           <img
-            src="https://img.icons8.com/color/144/000000/nodejs.png"
-            alt="Node.js"
+            src='https://img.icons8.com/color/144/000000/nodejs.png'
+            alt='Node.js'
           />
         </div>
-        <div className="column">
+        <div className='column'>
           <img
-            src="https://img.icons8.com/color/144/000000/mongodb.png"
-            alt="MongoDB"
+            src='https://img.icons8.com/color/144/000000/mongodb.png'
+            alt='MongoDB'
           />
         </div>
-        <div className="column">
+        <div className='column'>
           <img
-            src="https://img.icons8.com/nolan/128/api-settings.png"
-            alt="REST API"
+            src='https://img.icons8.com/nolan/128/api-settings.png'
+            alt='REST API'
           />
         </div>
       </div>
 
-      <Link to="/projects" className="button">
-        <i className="fas fa-laptop-code"></i> View My Work
+      <Link to='/projects' className='button'>
+        <i className='fas fa-laptop-code'></i> View My Work
       </Link>
     </div>
   );
